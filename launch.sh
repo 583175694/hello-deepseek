b2c1d34567575f305e6540214b6228bed82dda05
583175694 <10918338+w583175694@user.noreply.gitee.com>
2025-02-22 00:33:52 +0800

    chore: 更新脚本

--- launch.sh
+++ launch.sh
@@ -1,18 +1,16 @@
+# 更新代码
+git pull
 # 客户端
 cd client
-
 # 重新安装依赖
 npm install
-
 # 重新构建
 npm run build
-
 # 重启服务
 pm2 delete chat-client
 pm2 start npm --name chat-client -- start
 
 # 服务端
-# 返回上级目录
 cd ..
 # 进入服务端目录
 cd server
