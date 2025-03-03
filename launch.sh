82eecdc935142e7249a31680a65d736896026457
583175694 <10918338+w583175694@user.noreply.gitee.com>
2025-03-03 20:36:55 +0800

    chore: 构建问题

--- launch.sh
+++ launch.sh
@@ -3,9 +3,9 @@ git pull
 # 客户端
 cd client
 # 重新安装依赖
-# npm install --legacy-peer-deps
+npm install --legacy-peer-deps
 # 重新构建
-# npm run build
+npm run build
 # 重启服务
 pm2 delete chat-client
 pm2 start npm --name chat-client -- start
@@ -15,9 +15,9 @@ cd ..
 # 进入服务端目录
 cd server
 # 重新安装依赖
-# npm install --legacy-peer-deps
+npm install --legacy-peer-deps
 # 重新构建
-# npm run build
+npm run build
 # 删除旧的服务端进程
 pm2 delete chat-server
 # 启动新的服务端进程
